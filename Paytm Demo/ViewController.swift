4618634feed2a0aec27da6ada1b24522fadd16ca
Purushottam Padhya <purushottam.padhya@newitventure.com>
2018-07-04 14:59:37 +0545

    Initial Commit

---
+++ Paytm Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paytm Demo
+//
+//  Created by NITV on 7/4/18.
+//  Copyright © 2018 NITV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
