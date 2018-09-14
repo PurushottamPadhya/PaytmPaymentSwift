9416424c99e6889d2e6d00af68c31533cda8fb92
Purushottam Padhya <purushottam.padhya@newitventure.com>
2018-09-14 12:55:14 +0545

    paytm integration setup

---
+++ Paytm Demo/New Group/PaytmManager.swift
@@ -0,0 +1,16 @@
+//
+//  PaytmHelper.swift
+//  Paytm Demo
+//
+//  Created by NITV on 7/4/18.
+//  Copyright © 2018 NITV. All rights reserved.
+//
+
+import UIKit
+
+class PaytmHelper: NSObject {
+
+    
+    
+    
+}
---
+++ Paytm Demo/Paytm Demo-bridging-header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  Paytm Demo
+//
+//  Created by NITV on 7/4/18.
+//  Copyright © 2018 NITV. All rights reserved.
+//
+#import "PaymentsSDK.h"
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
